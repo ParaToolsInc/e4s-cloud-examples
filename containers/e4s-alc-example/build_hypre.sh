514feff338ff53c75b0467c8f4eecb3c468768aa
Sameer Shende <sameer@cs.uoregon.edu>
2024-05-03 10:07:10 -0700

    Updated hypre example

---
+++ containers/e4s-alc-example/build_hypre.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+### Set up e4s-alc to be used ###
+spack load --first e4s-alc
+E4S_ALC=`spack location -i --first e4s-alc`/bin/e4s-alc
+
+### Pulling image and writing the definition file ###
+$E4S_ALC create -f hypre-build-specs.yaml
+ls $HOME/.e4s-alc/singularity_images
+cat singularity.def
+
+### Building the new image ###
+singularity build --fakeroot ubuntu20.04_hypre.sif singularity.def
+
+
