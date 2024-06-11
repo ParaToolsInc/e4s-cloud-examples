4b40e90778aaebf526c05704c00c0cb4e0ee69c6
Sameer Shende <sameer@cs.uoregon.edu>
2024-06-11 10:47:10 -0700

    Adding batch files for SLURM and Torque.

---
+++ containers/e4s-cl-example/run_e4s-cl_mvapich_qsub.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#module load mvapich2-x-aws
+# Uncomment and use the correct mvapich2 module name.
+
+spack load e4s-cl
+MPI=$(which mpirun | awk -F'/bin/mpirun' '{print $1}')
+
+echo "Using MPI from: ${MPI}"
+
+e4s-cl profile delete \#
+
+e4s-cl init --mpi ${MPI} --profile mvapich --backend singularity --image `pwd`/ubuntu20.04_hypre.sif --source ./source.sh
+
+qsub hypre_test.qsub
+#e4s-cl mpirun -np 8 ./hypre_test -P 2 2 2 -n 100 100 100 
+
+e4s-cl profile list mvapich
+
---
+++ containers/e4s-cl-example/run_e4s-cl_openmpi_sbatch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+module load openmpi
+
+spack load e4s-cl
+MPI=$(which mpirun | awk -F'/bin/mpirun' '{print $1}')
+
+echo "Using MPI from: ${MPI}"
+
+e4s-cl profile delete \#
+
+e4s-cl init --mpi ${MPI} --profile ompi  --backend singularity --image `pwd`/ubuntu20.04_hypre.sif --source ./source.sh
+
+sbatch hypre_test.sbatch
+#e4s-cl --from mpich mpirun -np 8 ./hypre_test -P 2 2 2 -n 100 100 100 
+
+e4s-cl profile list ompi
