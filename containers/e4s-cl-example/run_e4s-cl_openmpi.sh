58fda565e173ad3a168458a8bb79ad87c78cc722
eugeneswalker <eugenesunsetwalker@gmail.com>
2024-08-28 23:16:20 +0000

    properly initialize openmpi

--- containers/e4s-cl-example/run_e4s-cl_openmpi.sh
+++ containers/e4s-cl-example/run_e4s-cl_openmpi.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-module load openmpi
+module unload mpi
+module load mpi/openmpi
 
-spack load e4s-cl
+E4SCL=$(spack find --format /{hash:7} e4s-cl | head -c7)
+PYTHON_HASH=$(spack dependencies -it ${E4SCL} | grep python@ | cut -d' ' -f1)
+export PATH=$(spack location -i /$PYTHON_HASH)/bin:$PATH
+export PYTHONPATH=$(spack load --sh ${E4SCL} | grep PYTHONPATH | cut -d= -f2)
+export PATH=$(spack location -i ${E4SCL})/bin:$PATH
+which e4s-cl
+
 MPI=$(which mpirun | awk -F'/bin/mpirun' '{print $1}')
 
 echo "Using MPI from: ${MPI}"
 
-e4s-cl init --mpi ${MPI} --profile ompi  --backend singularity --image `pwd`/ubuntu20.04_hypre.sif --source ./source.sh
+e4s-cl profile delete \#
 
-e4s-cl --from mpich mpirun -np 8 ./hypre_test -P 2 2 2 -n 100 100 100 
+e4s-cl init --mpi ${MPI} --launcher_args "-x LD_LIBRARY_PATH=$LD_LIBRARY_PATH" --profile ompi  --backend singularity --image `pwd`/ubuntu20.04_hypre.sif --source ./source.sh
 
-e4s-cl profile list ompi
-
-e4s-cl profile delete ompi
+e4s-cl --from mpich mpirun -x LD_LIBRARY_PATH -x PYTHONPATH ./hypre_test -P 2 2 2 -n 100 100 100
