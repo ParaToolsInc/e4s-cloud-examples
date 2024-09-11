7c1391dd80fc61a451af08911388918ea7f467d9
eugeneswalker <eugenesunsetwalker@gmail.com>
2024-09-11 16:45:22 +0000

    fetch modules.yaml with curl -k

--- containers/e4s-alc-example/build_hypre.sh
+++ containers/e4s-alc-example/build_hypre.sh
@@ -7,6 +7,7 @@ E4S_ALC=`spack location -i --first e4s-alc`/bin/e4s-alc
 ### Pulling image and writing the definition file ###
 $E4S_ALC create -f hypre-build-specs.yaml
 ls $HOME/.e4s-alc/singularity_images
+sed -i -e 's/curl https/curl -k https/' singularity.def
 cat singularity.def
 
 ### Building the new image ###
