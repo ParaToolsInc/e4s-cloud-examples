c294f4e8826959eb4289605cfc1911f7eccfd82e
eugeneswalker <eugenesunsetwalker@gmail.com>
2025-08-14 17:17:24 -0700

    updates for e4s-alc example

--- containers/e4s-alc-example/build_hypre.sh
+++ containers/e4s-alc-example/build_hypre.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+in_path () {
+  type "$1" >/dev/null 2>/dev/null
+}
+
 ### Set up e4s-alc to be used ###
-spack load --first e4s-alc
-E4S_ALC=`spack location -i --first e4s-alc`/bin/e4s-alc
+if ! in_path e4s-alc ; then
+  spack load --first e4s-alc
+  E4S_ALC=`spack location -i --first e4s-alc`/bin/e4s-alc
+else
+  E4S_ALC=$(which e4s-alc)
+fi
 
 ### Pulling image and writing the definition file ###
 $E4S_ALC create -f hypre-build-specs.yaml
